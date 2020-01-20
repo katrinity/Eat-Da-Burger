56538e001824fce81f8632d7184597ecf225e5a5
K G <katrinity1@yahoo.com>
2020-01-19 18:30:53 -0800

    db setup

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INT NOT NULL AUTO_INCREMENT, 
+burger_name VARCHAR(500) NOT NULL,
+devoured BOOLEAN NOT NULL DEFAULT 0, 
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Salmon Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Mashroom Bacon Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
